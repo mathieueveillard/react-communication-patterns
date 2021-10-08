b41da13fa2b2cf5f9ab62a8c9c226ae5085f8bd5
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2021-10-08 12:52:36 +0200

    Problem statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Alice from "./Alice";
+import Bob from "./Bob";
 
-function App() {
+function App(): React.ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div style={{ padding: "4rem" }}>
+        <Alice />
+      </div>
+      <div style={{ padding: "4rem" }}>
+        <Bob />
+      </div>
     </div>
   );
 }
